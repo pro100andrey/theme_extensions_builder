7670a19d0dee1b28461f987a470d77f6b00b54f7
Andrii Ivanov <thsis.andrey@gmail.com>
2025-08-03 18:14:26 +0300

    wip: update deps

--- packages/theme_extensions_builder/lib/src/generator/theme_extensions_builder_generator.dart
+++ packages/theme_extensions_builder/lib/src/generator/theme_extensions_builder_generator.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
-import 'package:analyzer/dart/element/element.dart';
-import 'package:analyzer/dart/element/visitor.dart';
+import 'package:analyzer/dart/element/element2.dart';
 import 'package:build/build.dart';
 import 'package:dart_style/dart_style.dart';
 import 'package:source_gen/source_gen.dart';
@@ -22,11 +21,11 @@ class ThemeExtensionsGenerator extends GeneratorForAnnotation<ThemeExtensions> {
 
   @override
   Future<String> generateForAnnotatedElement(
-    Element element,
+    Element2 element,
     ConstantReader annotation,
     BuildStep buildStep,
   ) async {
-    if (element is! ClassElement || element is Enum) {
+    if (element is! ClassElement2 || element is Enum) {
       throw InvalidGenerationSourceError(
         'ThemeExtensions can only annotate classes',
         element: element,
@@ -35,7 +34,7 @@ class ThemeExtensionsGenerator extends GeneratorForAnnotation<ThemeExtensions> {
     }
 
     final classVisitor = _ClassVisitor();
-    element.visitChildren(classVisitor);
+    element.visitChildren2(classVisitor);
 
     final buildContext = annotation.read('buildContextExtension').boolValue;
 
@@ -44,7 +43,7 @@ class ThemeExtensionsGenerator extends GeneratorForAnnotation<ThemeExtensions> {
         ThemeMixinTemplate(
           ThemeMixinConfig(
             fields: classVisitor.fields,
-            className: element.name,
+            className: element.displayName,
           ),
         ),
       );
@@ -52,9 +51,7 @@ class ThemeExtensionsGenerator extends GeneratorForAnnotation<ThemeExtensions> {
     if (buildContext) {
       generatorBuffer.write(
         BuildContextTemplate(
-          BuildContextConfig(
-            className: element.name,
-          ),
+          BuildContextConfig(className: element.displayName),
         ),
       );
     }
@@ -71,47 +68,53 @@ class ThemeExtensionsGenerator extends GeneratorForAnnotation<ThemeExtensions> {
 
 /// It's a class that extends the SimpleElementVisitor class, and it overrides
 /// the visitClassElement method
-class _ClassVisitor extends SimpleElementVisitor<void> {
+class _ClassVisitor extends ElementVisitor2<void> {
   final Map<String, Field> fields = {};
   final Map<String, List<bool>> hasInternalAnnotations = {};
 
-  final ignoreAnnotationTypeChecker =
-      TypeChecker.fromRuntime(ignore.runtimeType);
+  final ignoreAnnotationTypeChecker = TypeChecker.fromRuntime(
+    ignore.runtimeType,
+  );
 
   @override
-  Future<void> visitFieldElement(FieldElement element) async {
+  Future<void> visitFieldElement(FieldElement2 element) async {
     if (ignoreAnnotationTypeChecker.hasAnnotationOf(element)) {
       return;
     }
 
     if (element.isFinal) {
-      fields[element.name] = Field(
+      fields[element.displayName] = Field(
         hasLerp: _hasLerp(element),
-        name: element.name,
+        name: element.displayName,
         typeName: element.type.getDisplayString(),
       );
     }
   }
 
-  bool _hasLerp(FieldElement field) {
-    final element = field.type.element;
+  bool _hasLerp(FieldElement2 field) {
+    final element = field.type.element3;
 
-    if (element is! ClassElement) {
+    if (element is! ClassElement2) {
       return false;
     }
 
-    for (final type in [
+    final types = [
       element,
       ...element.allSupertypes
           .where((e) => !e.isDartCoreObject)
-          .map((e) => e.element),
-    ]) {
-      for (final method in type.methods) {
-        if (method.name == 'lerp' &&
+          .map((e) => e.element3),
+    ];
+
+    for (final type in types) {
+      for (final method in type.methods2) {
+        if (method.displayName == 'lerp' &&
             method.isStatic &&
-            method.isPublic &&
-            method.parameters.length == 3 &&
-            method.parameters.last.type.isDartCoreDouble) {
+            method.isPublic) {
+          final last = method.children2.last;
+          if (last is FormalParameterElement) {
+            return last.type.isDartCoreDouble;
+          }
+
           return true;
         }
       }
@@ -119,4 +122,73 @@ class _ClassVisitor extends SimpleElementVisitor<void> {
 
     return false;
   }
+
+  @override
+  void visitClassElement(ClassElement2 element) {}
+
+  @override
+  void visitConstructorElement(ConstructorElement2 element) {}
+
+  @override
+  void visitEnumElement(EnumElement2 element) {}
+
+  @override
+  void visitExtensionElement(ExtensionElement2 element) {}
+
+  @override
+  void visitExtensionTypeElement(ExtensionTypeElement2 element) {}
+
+  @override
+  void visitFieldFormalParameterElement(FieldFormalParameterElement2 element) {}
+
+  @override
+  void visitFormalParameterElement(FormalParameterElement element) {}
+
+  @override
+  void visitGenericFunctionTypeElement(GenericFunctionTypeElement2 element) {}
+
+  @override
+  void visitGetterElement(GetterElement element) {}
+
+  @override
+  void visitLabelElement(LabelElement2 element) {}
+
+  @override
+  void visitLibraryElement(LibraryElement2 element) {}
+
+  @override
+  void visitLocalFunctionElement(LocalFunctionElement element) {}
+
+  @override
+  void visitLocalVariableElement(LocalVariableElement2 element) {}
+
+  @override
+  void visitMethodElement(MethodElement2 element) {}
+
+  @override
+  void visitMixinElement(MixinElement2 element) {}
+
+  @override
+  void visitMultiplyDefinedElement(MultiplyDefinedElement2 element) {}
+
+  @override
+  void visitPrefixElement(PrefixElement2 element) {}
+
+  @override
+  void visitSetterElement(SetterElement element) {}
+
+  @override
+  void visitSuperFormalParameterElement(SuperFormalParameterElement2 element) {}
+
+  @override
+  void visitTopLevelFunctionElement(TopLevelFunctionElement element) {}
+
+  @override
+  void visitTopLevelVariableElement(TopLevelVariableElement2 element) {}
+
+  @override
+  void visitTypeAliasElement(TypeAliasElement2 element) {}
+
+  @override
+  void visitTypeParameterElement(TypeParameterElement2 element) {}
 }
